17f6d8a0b6fa34eb22910ef10d4e670a4f1fe6ff
Fachri Hawari <fachri.hawari@gmail.com>
2024-07-16 13:53:48 +0700

    feat: Register done

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,11 @@ class UserController {
   static async login(req, res) {
     res.json({ access_token: "login" })
   }
+  static async register(req, res) {
+    res.json({ message: "User registerd successfully" })
+  }
 }
 
 module.exports = UserController
+
+
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const UserController = require('../controllers/UserController')
 const router = require('express').Router()
 
 router.post('/login', UserController.login)
+router.post('/register', UserController.register)
 
 module.exports = router
